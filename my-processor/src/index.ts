6c35a439ad0c06bf92bf31410d808c9780d1801c
shadowWalker <hieu811@gmail.com>
2024-08-01 15:17:51 +0700

    init project

---
+++ my-processor/src/index.ts
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+/*
+ * Copyright (C) 2023 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster
+ *
+ * This file is part of nodejs-processor
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Processor, { MessageRequest, Response } from "@routr/processor";
+import { Method } from "@routr/common";
+import { getLogger } from "@fonoster/logger";
+import { LocationClient } from "@routr/location";
+import { registerHandler, messageHandler } from "./handlers";
+import { BIND_ADDR, LOCATION_ADDR } from "./envs";
+
+const locationClient = new LocationClient({ addr: LOCATION_ADDR });
+
+const logger = getLogger({
+  service: "instant-messaging",
+  filePath: module.filename
+});
+
+new Processor({ bindAddr: BIND_ADDR, name: "instant-messaging" }).listen(
+  async (req: MessageRequest, res: Response) => {
+    try {
+      switch (req.method.toString()) {
+        case Method.REGISTER:
+          return registerHandler(locationClient, req, res);
+        case Method.MESSAGE:
+          return messageHandler(locationClient, req, res);
+        default:
+          res.sendNotImplemented();
+      }
+    } catch (err) {
+      logger.error("An error occurred:", { error: err.message });
+      res.sendInternalServerError();
+    }
+  }
+);
